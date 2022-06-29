135dada959736e248e967f0cc4976964272c48d8
sergeybarzul <96749365+sergeycodes@users.noreply.github.com>
2022-06-28 19:06:52 -0700

    worked on welcome page

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const primaryNav = document.querySelector('.primary-navigation')
 const NavToggle = document.querySelector('.mobile-nav-toggle')
-
+const welcomeEl = document.querySelector('.welcome-container')
+const projectEl = document.querySelector('.project-container')
+const navbar = document.querySelector('.navbar')
 
 NavToggle.addEventListener('click', ()=> {
     const visibility = primaryNav.getAttribute('data-visible')
@@ -13,3 +15,27 @@ NavToggle.addEventListener('click', ()=> {
         NavToggle.setAttribute("aria-expanded", false)
     }
 })
+
+var inverval_timer;
+
+//Time in milliseconds [1 second = 1000 milliseconds ]    
+inverval_timer = setInterval(function() { 
+    console.log("5 seconds completed");
+    welcomeMessage()
+}, 5000);
+    
+//IF you want to stop above timer
+function stop_timer() {
+    clearInterval(inverval_timer); 
+}
+
+function welcomeMessage() {
+    welcomeEl.style.transition = "all 1s"
+    welcomeEl.style.width = "80vw"
+    welcomeEl.style.height = "80vh"
+    welcomeEl.style.marginTop = "11vh"
+    welcomeEl.style.marginLeft = "10vw"
+    projectEl.style.transition = "all 1s"
+    projectEl.style.marginTop = "100vh"
+    navbar.style.top = "0"
+}
